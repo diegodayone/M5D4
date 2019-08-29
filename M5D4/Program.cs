561747e2eeb2af738a260b7c78d1aa99ae8f6562
Diego Banovaz <diego@strive.school>
2019-08-29 10:37:30 +0200

    Add project files.

---
+++ M5D4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M5D4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
